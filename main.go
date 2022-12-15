0ad3cdb57538b204c9fe6a42f6080c030272255d
Daniel Rojo <danielrojo@gmail.com>
2022-12-15 13:02:23 +0100

    structure for trivia

--- main.go
+++ main.go
@@ -16,13 +16,20 @@ func main() {
 			Value: "today",
 			Usage: "date to get data for (format: YYYY-MM-DD) (default: today)",
 		},
+		cli.StringFlag{
+			Name:  "trivia",
+			Value: "today",
+			Usage: "Answer a question about anything",
+		},
 	}
 	app.Action = func(c *cli.Context) error {
-		if apod.IsValidDate(c.String("date")) {
-			fmt.Println("Valid date: ", c.String("date"))
+		if apod.IsValidDate(c.String("apod")) {
+			fmt.Println("Valid date: ", c.String("apod"))
 			apod := apod.Apod{}
 			apod = apod.GetApod(c.String("date"))
 			fmt.Println(apod.Hdurl)
+		} else if c.String("trivia") != "" { // trivia parameter is a number
+			fmt.Println("Today's trivia")
 		} else {
 			fmt.Println("Invalid date: ", c.String("date"))
 		}
